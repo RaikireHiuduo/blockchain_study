bb77ba6b1dbb087654e5d32d9c1b346155e85d2d
RaikireHiuduo <chun_heng@hotmail.com>
2018-02-02 15:29:26 +0800

    start multi-org test

---
+++ demo/composerTutorial/release/1.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Clone
+git clone -b issue-6978 https://github.com/sstone1/fabric-samples.git
+
+# Get and update all the Hyperledger Fabric files. Eats a lot of bandwidth and space.
+cd ../fabric-samples
+curl -sSL https://goo.gl/byy2Qj | bash -s 1.0.5
+
+# Remove everything from the docker process, including stopped ones. 
+docker ps -q | xargs docker stop
+docker ps -qa | xargs docker rm
+
+# Up
+cd ./first-network
+./byfn.sh -m generate
+./byfn.sh -m up -s couchdb -a
+
+# Remove card
+composer card delete -n PeerAdmin@byfn-network-org1-only
+composer card delete -n PeerAdmin@byfn-network-org1
+composer card delete -n PeerAdmin@byfn-network-org2-only
+composer card delete -n PeerAdmin@byfn-network-org2
+composer card delete -n alice@tutorial-network
+composer card delete -n bob@tutorial-network
+composer card delete -n admin@tutorial-network
+composer card delete -n PeerAdmin@fabric-network
+
+# Return to main
+cd ../../release
---
+++ demo/composerTutorial/release/README.md
@@ -0,0 +1,8 @@
+# release
+
+https://hyperledger.github.io/composer/unstable/tutorials/deploy-to-fabric-multi-org.html
+
+docker ps -q | xargs docker stop
+docker ps -qa | xargs docker rm
+
+tba: this is messy version. check [1](1.sh), [2](2.sh), or [test](test.sh) for now.
